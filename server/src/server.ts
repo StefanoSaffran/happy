a2055208395123b7789a5d90748aa3856d022177
Stefano Saffran <stefanosaffran@hotmail.com>
2020-10-13 14:57:36 -0400

    :recycle: chore: add routes/errorhandler/database-connection/cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,21 @@
 import express from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import path from 'path';
+
+import './database/connection';
+
+import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 
-app.get('/users', () => {
-  console.log('teste');
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('🚀 Server started! Port: 3333!');
+});
