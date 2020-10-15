6b6d18bae83a2293916b4bb98bb5c56354ca823c
Stefano Saffran <stefanosaffran@hotmail.com>
2020-10-15 10:17:27 -0400

    :recycle: chore: refactoring App.tsx adding routes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,46 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View, Dimensions, Text } from 'react-native';
-import MapView,{ Marker, Callout, PROVIDER_GOOGLE }  from "react-native-maps";
+import { AppLoading } from "expo";
+import { useFonts } from "expo-font";
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from "@expo-google-fonts/nunito";
 
-import mapMarker from './src/assets/map-marker.png';
+import Routes from './src/routes';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  });
+
+  if (!fontsLoaded) return <AppLoading />;
+
   return (
-    <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map} 
-        initialRegion={{
-          latitude: -21.6157355,
-          longitude: -55.1698906,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008,
-        }}
-      >
-        <Marker 
-          icon={mapMarker} 
-          coordinate={{
-            latitude: -21.6157355,
-            longitude: -55.1698906,
-          }} 
-        >
-          <Callout>
-            <Text>Lar das crianças</Text>
-          </Callout>
-        </Marker>
-      </MapView>
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  }
-});
