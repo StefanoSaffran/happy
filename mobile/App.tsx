837bbcea641efe403bd83c2fd674dacf005faf8f
Stefano Saffran <stefanosaffran@hotmail.com>
2020-10-15 09:29:55 -0400

    :tada: Initial Structure

---
+++ mobile/App.tsx
@@ -0,0 +1,46 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, View, Dimensions, Text } from 'react-native';
+import MapView,{ Marker, Callout, PROVIDER_GOOGLE }  from "react-native-maps";
+
+import mapMarker from './src/assets/map-marker.png';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map} 
+        initialRegion={{
+          latitude: -21.6157355,
+          longitude: -55.1698906,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+      >
+        <Marker 
+          icon={mapMarker} 
+          coordinate={{
+            latitude: -21.6157355,
+            longitude: -55.1698906,
+          }} 
+        >
+          <Callout>
+            <Text>Lar das crianças</Text>
+          </Callout>
+        </Marker>
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
+});
